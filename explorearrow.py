df5dd16e42d2fa39aec7a1282d6873240c454fa0
Archanciel <jp.schnyder@gmail.com>
2017-10-04 23:52:59 +0200

    init commit. Solve my date time problem.

---
+++ explorearrow.py
@@ -0,0 +1,44 @@
+import arrow
+from dateutil import tz
+
+DATE_TIME_FORMAT_ARROW = 'YYYY/MM/DD HH:mm:ss'
+DATE_TIME_FORMAT_TZ_ARROW = DATE_TIME_FORMAT_ARROW + ' ZZ'
+SUMMER_MONTH = '09'
+WINTER_MONTH = '11'
+
+indiraOrderDateStr = '2017/' + SUMMER_MONTH + '/30 21:31:55'
+print("\n-- Placing summer order --")
+orderArrowObjIN = arrow.get(indiraOrderDateStr, DATE_TIME_FORMAT_ARROW).replace(tzinfo='Asia/Calcutta')
+orderArrowObjUTC = orderArrowObjIN.to(tz.gettz('UTC'))
+timeStampUTC = orderArrowObjUTC.timestamp
+print('IN:  ' + orderArrowObjIN.format(DATE_TIME_FORMAT_TZ_ARROW))
+print('UTC: ' + orderArrowObjUTC.format(DATE_TIME_FORMAT_TZ_ARROW))
+print('TS:  ' + str(timeStampUTC) + '-utc ' + str(orderArrowObjIN.timestamp) + '-in')
+
+print("\n-- Browsing summer order --")
+orderArrowObjUTC = arrow.get(timeStampUTC).replace(tzinfo='UTC')
+orderArrowObjIN = orderArrowObjUTC.to('Asia/Calcutta')
+#orderArrowObjIN = arrow.Arrow.utcfromtimestamp(timeStampUTC).to('Asia/Calcutta') works too !
+print('UTC: ' + orderArrowObjUTC.format(DATE_TIME_FORMAT_TZ_ARROW))
+print('IN:  ' + orderArrowObjIN.format(DATE_TIME_FORMAT_TZ_ARROW))
+print('TS:  ' + str(timeStampUTC) + '-utc ' + str(orderArrowObjIN.timestamp) + '-in')
+
+indiraOrderDateStr = '2017/' + WINTER_MONTH + '/30 21:31:55'
+print("\n-- Placing winter order --")
+orderArrowObjIN = arrow.get(indiraOrderDateStr, DATE_TIME_FORMAT_ARROW).replace(tzinfo='Asia/Calcutta')
+orderArrowObjUTC = orderArrowObjIN.to(tz.gettz('UTC'))
+timeStampUTC = orderArrowObjUTC.timestamp
+print('IN:  ' + orderArrowObjIN.format(DATE_TIME_FORMAT_TZ_ARROW))
+print('UTC: ' + orderArrowObjUTC.format(DATE_TIME_FORMAT_TZ_ARROW))
+print('TS:  ' + str(timeStampUTC) + '-utc ' + str(orderArrowObjIN.timestamp) + '-in')
+
+print("\n-- Browsing winter order --")
+orderArrowObjUTC = arrow.get(timeStampUTC).replace(tzinfo='UTC')
+orderArrowObjIN = orderArrowObjUTC.to('Asia/Calcutta')
+#orderArrowObjIN = arrow.Arrow.utcfromtimestamp(timeStampUTC).to('Asia/Calcutta') works too !
+print('UTC: ' + orderArrowObjUTC.format(DATE_TIME_FORMAT_TZ_ARROW))
+print('IN:  ' + orderArrowObjIN.format(DATE_TIME_FORMAT_TZ_ARROW))
+print('TS:  ' + str(timeStampUTC) + '-utc ' + str(orderArrowObjIN.timestamp) + '-in')
+
+if __name__ == '__main__':
+    pass
