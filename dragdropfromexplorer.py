d27540e8a8ca814084573225d18808deda2fe834
Jean-Pierre Schnyder <jp.schnyder@gmail.com>
2018-04-21 22:12:40 +0200

    not useful

--- dragdropfromexplorer.py
+++ dragdropfromexplorer.py
@@ -1,11 +1,6 @@
 from kivy.app import App
 from kivy.core.window import Window
 
-import os
-os.environ['KIVY_GL_BACKEND'] = 'angle_sdl2'
-from kivy import Config
-Config.set('graphics', 'multisamples', '0')
-
 class WindowFileDropExampleApp(App):
     def build(self):
         Window.bind(on_dropfile=self._on_file_drop)
