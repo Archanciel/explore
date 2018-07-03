497a242c4ecbb6f2d7e0c2c0de4bc080363a6ae1
Jean-Pierre Schnyder <jp.schnyder@gmail.com>
2018-07-03 22:26:03 +0200

    reading in c2 primary file

--- esspandamatplotlibpricevolume.py
+++ esspandamatplotlibpricevolume.py
@@ -5,16 +5,16 @@ import pandas as pd
 import pandas_datareader.data as web
 style.use('ggplot')
 
-df = pd.read_csv('tsla.csv', parse_dates=True, index_col=0)
+df = pd.read_csv('primary.csv', index_col=0)
 #df['100ma'] = df['Close'].rolling(window=100, min_periods=0).mean()
 print(df.head())
 
 ax1 = plt.subplot2grid((6,1), (0,0), rowspan=5, colspan=1)
 ax2v = plt.subplot2grid((6,1), (5,0), rowspan=1, colspan=1, sharex=ax1)
-ax2v.set_ylim(0, 1 * df['Volume'].max())
+ax2v.set_ylim(0, 1 * df['VOLUME'].max())
 
-ax1.plot(df.index, df['Close'])
+ax1.plot(df.index, df['PRICE'])
 #ax1.plot(df.index, df['100ma'])
-ax2v.bar(df.index, df['Volume'])
+ax2v.bar(df.index, df['VOLUME'])
 
 plt.show()
