a485e9330373b322f81ea17e7822a42b74cb1fe8
Jean-Pierre Schnyder <jp.schnyder@gmail.com>
2018-07-06 18:57:08 +0200

    optimize import

--- esspandamatplotlibpricevolume.py
+++ esspandamatplotlibpricevolume.py
@@ -1,8 +1,8 @@
-import datetime as dt
 import matplotlib.pyplot as plt
 from matplotlib import style
 import pandas as pd
-import pandas_datareader.data as web
+from matplotlib import style
+
 style.use('ggplot')
 
 df = pd.read_csv('primary.csv', index_col=0)
