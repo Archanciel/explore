6b8a3721440095ba179ca4fa9612c7a5cc34fd0d
Archanciel <jp.schnyder@gmail.com>
2018-08-14 21:38:18 +0200

    explored

--- intradaypricevolchart.py
+++ intradaypricevolchart.py
@@ -6,24 +6,25 @@ import pandas as pd
 import numpy as np
 from datetime import datetime
 
-n = 100
-idx = pd.date_range(start=datetime(2016, 1, 1, 10), freq='10Min', periods=n)
+n = 3600
+idx = pd.date_range(start=datetime.now(), freq='1S', periods=n)
 data = pd.DataFrame(data={'price': np.cumsum([0.0001] * n + np.random.random(n)),
-                          'volume': np.random.randint(low=100, high=10000, size=n)}, index=idx)
+                          'volume': np.random.randint(low=1, high=10000, size=n)}, index=idx)
+data = pd.read_csv('secondary-2018-08-12-21-32-56.csv', index_col=0, sep='\t')
+print(data.head())
+fig, ax = plt.subplots(nrows=2, sharex=True, figsize=(10,5))
 
-fig, ax = plt.subplots(nrows=2, sharex=True, figsize=(15,8))
+ax[0].plot(data.index, data['PRICE'])
+ax[1].bar(data.index, data['VOLUME'])
 
-ax[0].plot(data.index, data.price)
-ax[1].bar(data.index, data.volume, width=1/(5*len(data.index)))
+fmt = mpl.dates.DateFormatter('%M:%S')
+#ax[1].xaxis.set_major_locator(mpl.dates.HourLocator(interval=1))
+#aaaax[1].xaxis.set_major_formatter(xfmt)
 
-xfmt = mpl.dates.DateFormatter('%H:%M')
-ax[1].xaxis.set_major_locator(mpl.dates.HourLocator(interval=3))
-ax[1].xaxis.set_major_formatter(xfmt)
+#ax[1].xaxis.set_minor_locator(mpl.dates.HourLocator(interval=1))
+#aaaax[1].xaxis.set_minor_formatter(xfmt)
 
-ax[1].xaxis.set_minor_locator(mpl.dates.HourLocator(interval=1))
-ax[1].xaxis.set_minor_formatter(xfmt)
-
-ax[1].get_xaxis().set_tick_params(which='major', pad=25)
+ax[1].get_xaxis().set_tick_params(which='major', pad=15)
 
 fig.autofmt_xdate()
 plt.show()
