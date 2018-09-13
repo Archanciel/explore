2f0dbb3ec2c692c17dfd8d5eedeb09c57ce0a0f7
Archanciel <jp.schnyder@gmail.com>
2018-09-13 18:06:14 +0200

    init commit

---
+++ scatterdemo.py
@@ -0,0 +1,28 @@
+# ScatterDemo.py
+
+from kivy.uix.scatter import Scatter
+from kivy.uix.widget import Widget
+from kivy.uix.relativelayout import RelativeLayout
+from kivy.app import App
+
+
+class SquareWidget(Widget):
+    pass
+
+
+class ScatterWidget(Scatter):
+    pass
+
+
+class ScatterDemo(RelativeLayout):
+    pass
+
+
+class ScatterDemoApp(App):
+    def build(self):
+        return ScatterDemo()
+
+
+if __name__ == '__main__':
+    ScatterDemoApp().run()
+
