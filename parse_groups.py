8c958eb1a5c6436ca8b305f70927dbd508215329
Archanciel <jp.schnyder@gmail.com>
2019-08-09 19:38:13 +0200

    init commit

---
+++ parse_groups.py
@@ -0,0 +1,34 @@
+import re
+
+
+def parse(text, pattern):
+    listOfParagraphs = []
+
+    for match in re.finditer(pattern, text):
+        print('groups: ', match.groups())
+        print('\tsub groups:')
+        for subGroup in match.groups():
+            print('\t\t', subGroup)
+            listOfParagraphs.append(subGroup)
+
+    return listOfParagraphs
+
+
+def printList(list):
+    print('list: ', end='')
+    
+    for line in list:
+        print(line, end='')
+
+    print()
+
+
+text = "Hello world 475 times you said that for 2 people"
+pattern = r'(\D+)(\d+|.*)'
+
+print(text)
+print(pattern)
+print()
+
+list = parse(text, pattern)
+printList(list)
