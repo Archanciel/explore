c94e2f8cd4b81a91919bce7800d7a28c1fe0b752
Archanciel <jp.schnyder@gmail.com>
2020-08-31 16:27:35 +0200

    simplified and renaming

--- kivy_explore/kivyrecycleview_movebuttons.py
+++ kivy_explore/kivyrecycleview_movebuttons.py
@@ -1,7 +1,5 @@
 from kivy.app import App
-from kivy.clock import Clock
 from kivy.config import Config
-from kivy.properties import ObjectProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 
@@ -12,14 +10,12 @@ from kivy.uix.recycleboxlayout import RecycleBoxLayout
 from kivy.uix.recycleview.layout import LayoutSelectionBehavior
 from kivy.uix.recycleview.views import RecycleDataViewBehavior
 
-
 class SelectableRecycleBoxLayout(FocusBehavior, LayoutSelectionBehavior,
                                  RecycleBoxLayout):
     ''' Adds selection and focus behaviour to the view. '''
 
     # required to authorise unselecting a selected item
     touch_deselect_last = BooleanProperty(True)
-
 
 class SelectableLabel(RecycleDataViewBehavior, Label):
     ''' Add selection support to the Label '''
@@ -30,6 +26,7 @@ class SelectableLabel(RecycleDataViewBehavior, Label):
     def refresh_view_attrs(self, rv, index, data):
         ''' Catch and handle the view changes '''
         self.index = index
+
         return super(SelectableLabel, self).refresh_view_attrs(
             rv, index, data)
 
@@ -56,62 +53,42 @@ class SelectableLabel(RecycleDataViewBehavior, Label):
             rv.parent.parent.recycleViewCurrentSelIndex = index
 
 class KivyRecycleView(BoxLayout):
-    requestInput = ObjectProperty()
-    resultOutput = ObjectProperty()
-    showRequestList = False
-    currentSelIndex = -1
     recycleViewCurrentSelIndex = -1
 
     def __init__(self, **kwargs):
         super(KivyRecycleView, self).__init__(**kwargs)
 
         # setting RecycleView list item height from config
-        self.requestListRVSelBoxLayout.spacing = 0.5
         self.populateList()
 
     def populateList(self):
         for i in range(6):
             listEntry = {'text': 'line {}'.format(i)}
-            self.requestListRV.data.append(listEntry)
+            self.recycleViewList.data.append(listEntry)
 
-    def resetListViewScrollToEnd(self):
-        maxVisibleItemNumber = self.histoListMaxVisibleItems
-        listLength = len(self.requestListRV.data)
-
-        if listLength > maxVisibleItemNumber:
-            # for the moment, I do not know how to scroll to end of RecyclweView !
-            # listView.scroll_to(listLength - maxVisibleItemNumber)
-            pass
-        else:
-            if self.showRequestList:
-                listItemNumber = self.adjustRequestListSize()
-                if listItemNumber == 0:
-                    self.showRequestList = False
-                    self.manageStateOfRequestListButtons()
-
-    def moveUpRequest(self):
+    def moveUpSelItem(self):
         oldIndex = self.recycleViewCurrentSelIndex
         newIndex = oldIndex - 1
-        requestTotalNumber = len(self.requestListRV.data)
+        requestTotalNumber = len(self.recycleViewList.data)
 
         if newIndex < 0:
             # if first line request is moved up, it is moved at the end of the
             # request history list
             newIndex = requestTotalNumber - 1
 
-        self.moveItemInList(list=self.requestListRV.data, oldIndex=oldIndex, newIndex=newIndex)
+        self.moveItemInList(list=self.recycleViewList.data, oldIndex=oldIndex, newIndex=newIndex)
 
-    def moveDownRequest(self):
+    def moveDownSelItem(self):
         oldIndex = self.recycleViewCurrentSelIndex
         newIndex = oldIndex + 1
-        requestTotalNumber = len(self.requestListRV.data)
+        requestTotalNumber = len(self.recycleViewList.data)
 
         if newIndex == requestTotalNumber:
             # if first line request is moved up, it is moved at the end of the
             # request history list
             newIndex = 0
 
-        self.moveItemInList(list=self.requestListRV.data, oldIndex=oldIndex, newIndex=newIndex)
+        self.moveItemInList(list=self.recycleViewList.data, oldIndex=oldIndex, newIndex=newIndex)
 
     def moveItemInList(self, list, oldIndex, newIndex):
         list.insert(newIndex, list.pop(oldIndex))
@@ -125,16 +102,6 @@ class KivyRecycleView_moveButtonsApp(App):
         Config.write()
 
         return KivyRecycleView()
-
-
-    def on_pause(self):
-        # Here you can save data if needed
-        return True
-
-
-    def on_resume(self):
-        # Here you can check if any data needs replacing (usually nothing)
-        pass
 
 if __name__ == '__main__':
     dbApp = KivyRecycleView_moveButtonsApp()
