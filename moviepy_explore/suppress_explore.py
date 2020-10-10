e509d0e37fa0e6dfd5c1dba21cf143fab81b9294
Archanciel <jp.schnyder@gmail.com>
2020-10-10 20:01:54 +0200

    Update suppress_explore.py

--- moviepy_explore/suppress_explore.py
+++ moviepy_explore/suppress_explore.py
@@ -1,7 +1,3 @@
-import os, shutil
-
-from constants import *
-
 class AudioExtractor:
 	def suppressFrames(self, suppressFrameList):
 		suppressFrameNb = len(suppressFrameList)
@@ -10,12 +6,19 @@ class AudioExtractor:
 		
 		for extractIdx in range(suppressFrameNb + 1):
 			if extractIdx == 0:
-				clips.append([0, suppressFrameList[0][0]])
+				endValue = suppressFrameList[0][0]
+				
+				if endValue == 0:
+					# suppress frame is starting at 0 !
+					continue
+				
+				clips.append([0, endValue])
 			elif extractIdx == suppressFrameNb:
 				clips.append([suppressFrameList[extractIdx - 1][1], duration])
 			else:
 				clips.append([suppressFrameList[extractIdx - 1][1], suppressFrameList[extractIdx][0]])
 		
+		print('Time frames kept for suppress frame list{}:'.format(suppressFrameList))
 		print(clips)
 				
 if __name__ == "__main__":
@@ -25,4 +28,11 @@ if __name__ == "__main__":
 						 [15, 17]]
 						 
 	ae.suppressFrames(suppressFrameList)
-		
+
+	print()
+	
+	suppressFrameList = [[0, 8],
+	                     [11, 13],
+	                     [15, 17]]
+	
+	ae.suppressFrames(suppressFrameList)
