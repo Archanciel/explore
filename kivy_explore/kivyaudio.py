2c87c36b5fe8588ef03c5b769a604a6d11c94487
Archanciel <jp.schnyder@gmail.com>
2020-10-31 19:24:18 +0100

    init commit

---
+++ kivy_explore/kivyaudio.py
@@ -0,0 +1,25 @@
+import os
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.core.audio import SoundLoader
+
+class KivyAudio(BoxLayout):
+	def __init__(self, **kwargs):
+		super().__init__(**kwargs)
+
+	def play_sound(self):
+		if os.name == 'posix':
+			mp3PathFileName = '/storage/emulated/0/Download/Audiobooks/Malika/ess4.mp3'
+		else:
+			mp3PathFileName = 'D:\\Users\\Jean-Pierre\\Downloads\\Audiobooks\\Malika\\ess6.mp3'
+		sound = SoundLoader.load(mp3PathFileName)
+		if sound:
+			sound.play()
+
+class KivyAudioApp(App):
+
+	def build(self):
+		return KivyAudio()
+
+myApp = KivyAudioApp()
+myApp.run()
---
+++ kivy_explore/kivyaudiomp3.py
@@ -0,0 +1,30 @@
+import os
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.core.audio import SoundLoader
+import youtube_dl
+
+class KivyAudioMp3(App):
+    destDir = 'c:\\temp\\'
+    ydl_opts = {
+        'outtmpl': destDir + '%(title)s.%(ext)s',
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '128',
+        }],
+        'quiet': False
+    }
+    videoUrl = 'https://youtu.be/9iPvLx7gotk'
+    videoTitle = ''
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        meta = ydl.extract_info(videoUrl, download=False)
+        videoTitle = meta['title']
+        ydl.download([videoUrl])
+    sound=SoundLoader.load(destDir + videoTitle + '.mp3')
+    def build(self):
+        return Label(text="KivyAudioMp3 playing")
+    if sound:
+        sound.play()
+KivyAudioMp3().run()
