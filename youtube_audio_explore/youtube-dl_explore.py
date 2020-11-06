b6d6b87cd910196dbbc463f3d61e7a2f998d87f6
Archanciel <jp.schnyder@gmail.com>
2020-11-06 17:07:11 +0100

    added downloading a single video

--- youtube_audio_explore/youtube-dl_explore.py
+++ youtube_audio_explore/youtube-dl_explore.py
@@ -5,30 +5,42 @@ from pytube import Playlist
 if os.name == 'posix':
 	targetAudioDir = '/storage/emulated/0/Download/Audiobooks/test_youtube_dl'
 	ydl_opts = {
-	'outtmpl': targetAudioDir + '/%(title)s.mp3',
-	'format': 'bestaudio/best',
-	'quiet': False
+		'outtmpl': targetAudioDir + '/%(title)s.mp3',
+		#	'format': 'bestaudio/best',
+		'format': 'worstaudio/worst',
+		'quiet': False
 	}
 else:
 	targetAudioDir = 'D:\\Users\\Jean-Pierre\\Downloads\\Audiobooks\\test_youtube_dl'
 	ydl_opts = {
-	'outtmpl': targetAudioDir + '\\%(title)s.%(ext)s',
-	'format': 'bestaudio/best',
-	'postprocessors': [{
-						'key': 'FFmpegExtractAudio',
-						'preferredcodec': 'mp3',
-						'preferredquality': '128',
-					}],
+		'outtmpl': targetAudioDir + '\\%(title)s.%(ext)s',
+	#	'format': 'bestaudio/best',
+		'format': 'worstaudio/worst',
+		'postprocessors': [{
+							'key': 'FFmpegExtractAudio',
+							'preferredcodec': 'mp3',
+							'preferredquality': '96',
+						}],
 	'quiet': False
 	}
 
 playlistUrl = 'https://www.youtube.com/playlist?list=PLzwWSJNcZTMSFWGrRGKOypqN29MlyuQvn'
 playlistObject = Playlist(playlistUrl)
 playlistObject._video_regex = re.compile(r"\"url\":\"(/watch\?v=[\w-]*)")
-
+singlevideoUrl = 'https://youtu.be/Eqy6M6qLWGw'
+downloadPlaylist = False
 
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-	for videoUrl in playlistObject.video_urls:
-		meta = ydl.extract_info(videoUrl, download=False)
+	if downloadPlaylist:
+		for videoUrl in playlistObject.video_urls:
+			meta = ydl.extract_info(videoUrl, download=False)
+			videoTitle = meta['title']
+			ydl.download([videoUrl])
+			print('{} audio track downloaded'.format(videoTitle))
+	else:
+		# downloading single video
+		meta = ydl.extract_info(singlevideoUrl, download=False)
 		videoTitle = meta['title']
-		ydl.download([videoUrl])
+		ydl.download([singlevideoUrl])
+		print('{} audio track downloaded'.format(videoTitle))
+
