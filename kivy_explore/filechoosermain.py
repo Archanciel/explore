62af4a22f501cb5ef8610939b28e21b1f58e0e05
Archanciel <jp.schnyder@gmail.com>
2020-11-08 18:44:52 +0100

    working on Windows

--- kivy_explore/filechoosermain.py
+++ kivy_explore/filechoosermain.py
@@ -1,3 +1,4 @@
+import logging
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
 from kivy.config import Config
@@ -21,7 +22,7 @@ class SelectableRecycleBoxLayout(FocusBehavior, LayoutSelectionBehavior,
 	# required to authorise unselecting a selected item
 	touch_deselect_last = BooleanProperty(True)
 
-
+"""
 class SelectableLabel(RecycleDataViewBehavior, Label):
 	''' Add selection support to the Label '''
 	index = None
@@ -45,9 +46,50 @@ class SelectableLabel(RecycleDataViewBehavior, Label):
 		''' Respond to the selection of items in the view. '''
 		self.selected = is_selected
 		if is_selected:
-			print("selection changed to {0}".format(rv.data[index]))
+			logging.info("selection changed to {0}".format(rv.data[index]))
 		else:
-			print("selection removed for {0}".format(rv.data[index]))
+			logging.info("selection removed for {0}".format(rv.data[index]))
+"""
+
+class SelectableLabel(RecycleDataViewBehavior, Label):
+	''' Add selection support to the Label '''
+	index = None
+	selected = BooleanProperty(False)
+	selectable = BooleanProperty(True)
+
+	def refresh_view_attrs(self, rv, index, data):
+		''' Catch and handle the view changes '''
+		self.index = index
+		return super(SelectableLabel, self).refresh_view_attrs(
+			rv, index, data)
+
+	def on_touch_down(self, touch):
+		''' Add selection on touch down '''
+		if super(SelectableLabel, self).on_touch_down(touch):
+			return True
+		if self.collide_point(*touch.pos) and self.selectable:
+			# the parent is the containing SelectableRecycleBoxLayout.
+			# If the container is simply a RecycleBoxLayout,
+			# an error will occur since the RecycleBoxLayout has
+			# no method select_with_touch(), method which was
+			# added by the class LayoutSelectionBehavior
+			return self.parent.select_with_touch(self.index, touch)
+
+	def apply_selection(self, rv, index, is_selected):
+		''' Respond to the selection of items in the view. '''
+		if not self.selected and not is_selected:
+			# case when adding a new list item
+			logging.info('simply return {}'.format(index))
+			return
+		elif self.selected and not is_selected:
+			# toggling from selected to unselected
+			logging.info('handling deselection {}'.format(index))
+			self.selected = False
+			#rv.parent.parent.recycleViewSelectItem(index, is_selected)
+		else:
+			# toggling from unselected to selected
+			self.selected = not self.selected
+			logging.info('handling selection {}'.format(index))
 
 class LoadDialog(FloatLayout):
 	text_path_load = ObjectProperty(None)
@@ -65,12 +107,15 @@ class LoadDialog(FloatLayout):
 
 			for drive in available_drives:
 				self.drivesListRV.data.append({'text': drive})
+#			self.drivesListRV.data.append({'text': 'E:'})
 		else:
-			self.drivesListRV.data.append({'text': 'main'})	
+			self.drivesListRV.data.append({'text': 'main'})
 			self.drivesListRV.data.append({'text': 'sd card'})
-			#for i in range(4):
-			#	self.drivesListRV.data.append({'text': str(i)})		
-		#self.drivesListRV.data = [{'text': str(x)} for x in range(20)]
+			
+			# There's a dependency between the range size value and the height
+			# of list line !!!!
+			# for i in range(3):
+			# 	self.drivesListRV.data.append({'text': str(i)})
 
 class Root(FloatLayout):
 	def __init__(self, **kwargs):
