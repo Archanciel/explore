8effadfc1b5019c89cafd07b5a1c795eeb8f555a
Archanciel <jp.schnyder@gmail.com>
2021-03-27 15:35:51 +0100

    Update add_percent_col_and_total_row_for_selected_cols.py

--- pandas_explore/add_percent_col_and_total_row_for_selected_cols.py
+++ pandas_explore/add_percent_col_and_total_row_for_selected_cols.py
@@ -4,6 +4,6 @@ import numpy as np
 data = {'CAPITAL': [10000, 80000, 25000],
 		'REVENUE': [500, 4200, 2500]}
 df = pd.DataFrame(data=data)
-df.loc['TOTAL'] = df.sum(numeric_only=True, axis=0)[['CAPITAL', 'REVENUE']]
 df['YIELD %'] = df['REVENUE'] / df['CAPITAL'] * 100
-print(df)
+df.loc['TOTAL'] = df[['CAPITAL', 'REVENUE']].sum(numeric_only=True, axis=0)
+print(df.fillna(''))
