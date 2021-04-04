89e93a2347132c73ea517a22e943aefa7c42d716
Archanciel <jp.schnyder@gmail.com>
2021-04-04 23:30:02 +0200

    Create datetime_duplicate_explore.py

---
+++ pandas_explore/datetime_duplicate_explore.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import sys
+
+OWNER = 'OWNER'
+DEPWITHDR = 'DEP WITHDR'
+YIELD = 'YIELD AMT'
+DATE_TIME = 'DATE'
+TOTAL = 'TOTAL'
+
+# defining the dataframe
+df = pd.DataFrame({OWNER: 2 * ['Joe'] + 3 * ['Carla'] + ['Rob'],
+				   DATE_TIME: ['2021/01/01 00:00:00', '2021/01/02 00:00:00', '2021/01/03 00:00:00', '2021/01/03 00:00:00', '2021/01/05 00:00:00', '2021/01/06 00:00:00'],
+                   DEPWITHDR: [10000, 5000, 20000, 3000, -4000, 2000],
+                   YIELD: [1000, 500, 2000, 300, 400, 200]})
+
+print(df)
+print()
+
+duplSeries = df.duplicated(subset=[DATE_TIME])
+
+print(duplSeries)
+print()
+
+print('Is a datetime duplicated: ', duplSeries.any())
