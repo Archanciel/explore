65e2346afa1ec71741d9d550fea58ca91e14d55d
Archanciel <jp.schnyder@gmail.com>
2021-04-27 13:42:28 +0200

    Create pandas_average_rate_explore.py

---
+++ pandas_explore/pandas_average_rate_explore.py
@@ -0,0 +1,45 @@
+import pandas as pd
+
+CRYPTO = 'CHSB'
+DF_RATE = 'AT DF RT'
+CUR_RATE = 'AT CUR RT'
+POTENTIAL_CAP_GAIN = 'POT CAP G'
+AVERAGE_RATE = 'AVG RT'
+REALIZED_CAP_GAIN = 'REALI CAP G'
+YIELD_CHSB = 'Y CHSB'
+YIELD_CHF = 'Y CHF'
+
+df = pd.DataFrame({CRYPTO: [10000.0, 10000.0, 1000.0, -500.0, -500, 1000, -23200],
+				   DF_RATE: [5000.0, 10000.0, 2000.0, -500.0, -1000, 800, -41500],
+				   AVERAGE_RATE: [0.0] * 7,
+				   CUR_RATE: [15000.0, 15000.0, 1500.0, -750.0, -750, 1500, -34800],
+				   YIELD_CHSB: [0.0] * 7,
+				   YIELD_CHF: [0.0] * 7,
+				   REALIZED_CAP_GAIN: [0.0] * 7,
+				   POTENTIAL_CAP_GAIN: [0.0] * 7})
+				   
+print(df.to_string())
+currentTotalCrypto = 0
+currentTotalDfRate = 0
+currentTotalCurRate = 0
+currentAverageRate = 0
+
+for index, row in df.iterrows():
+	depWithdrAmount = row[CRYPTO]
+	currentTotalCrypto += depWithdrAmount
+	currentTotalCurRate += row[CUR_RATE]
+	
+	if depWithdrAmount > 0:
+		currentTotalDfRate += row[DF_RATE]
+		currentAverageRate = currentTotalDfRate / currentTotalCrypto
+		df.iloc[index][POTENTIAL_CAP_GAIN] = currentTotalCurRate - \
+											 (currentTotalCrypto * currentAverageRate)
+	else:
+		df.iloc[index][REALIZED_CAP_GAIN] = -1 * (row[DF_RATE] - \
+											depWithdrAmount * currentAverageRate)
+		df.iloc[index][POTENTIAL_CAP_GAIN] = currentTotalCurRate - \
+											 (currentTotalCrypto * currentAverageRate)
+				
+	df.iloc[index][AVERAGE_RATE] = currentAverageRate
+	
+print(df.to_string())
