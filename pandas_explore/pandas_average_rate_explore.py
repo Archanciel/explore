42de7b0a6acfee1ae8fb2aa881c7f965daf1c377
Archanciel <jp.schnyder@gmail.com>
2021-05-01 20:31:29 +0200

    in progress

--- pandas_explore/pandas_average_rate_explore.py
+++ pandas_explore/pandas_average_rate_explore.py
@@ -3,9 +3,11 @@ import pandas as pd
 CRYPTO = 'CHSB'
 DF_RATE = 'AT DF RT'
 CUR_RATE = 'AT CUR RT'
-POTENTIAL_CAP_GAIN = 'POT CAP G'
 AVERAGE_RATE = 'AVG RT'
 REALIZED_CAP_GAIN = 'REALI CAP G'
+REALIZED_CAP_GAIN_PERCENT = 'REALI CAP G %'
+POTENTIAL_CAP_GAIN = 'POT CAP G'
+POTENTIAL_CAP_GAIN_PERCENT = 'POT CAP G %'
 YIELD_CHSB = 'Y CHSB'
 YIELD_CUR_RATE = 'Y CUR RATE'
 
@@ -24,7 +26,9 @@ def computeDataFrameAmounts(sellType):
 						   YIELD_CHSB: [155.26, 163.82, 167.66, 170.62, 162.54, 712.55, 0.10],
 						   YIELD_CUR_RATE: [0.0] * 7,
 						   REALIZED_CAP_GAIN: [0.0] * 7,
-						   POTENTIAL_CAP_GAIN: [0.0] * 7})
+						   REALIZED_CAP_GAIN_PERCENT: [0.0] * 7,
+						   POTENTIAL_CAP_GAIN: [0.0] * 7,
+						   POTENTIAL_CAP_GAIN_PERCENT: [0.0] * 7})
 	elif sellType == SELL_ALL_DEPOSIT_SELL_ALL_MINUS_ONE_YIELD:
 		df = pd.DataFrame({CRYPTO: [10000.0, 10000.0, 1000.0, -500.0, -500, 1000, -22531.55],
 						   DF_RATE: [5000.0, 10000.0, 2000.0, -500.0, -1000, 800, -25200 - 1837.86],
@@ -33,7 +37,9 @@ def computeDataFrameAmounts(sellType):
 						   YIELD_CHSB: [155.26, 163.82, 167.66, 170.62, 162.54, 712.55, 0.10],
 						   YIELD_CUR_RATE: [0.0] * 7,
 						   REALIZED_CAP_GAIN: [0.0] * 7,
-						   POTENTIAL_CAP_GAIN: [0.0] * 7})
+						   REALIZED_CAP_GAIN_PERCENT: [0.0] * 7,
+						   POTENTIAL_CAP_GAIN: [0.0] * 7,
+						   POTENTIAL_CAP_GAIN_PERCENT: [0.0] * 7})
 	elif sellType == SELL_ALL_DEPOSIT_SELL_1000_YIELDS:
 		df = pd.DataFrame({CRYPTO: [10000.0, 10000.0, 1000.0, -500.0, -500, 1000, -22000],
 						   DF_RATE: [5000.0, 10000.0, 2000.0, -500.0, -1000, 800, -25200 - 1200],
@@ -42,7 +48,9 @@ def computeDataFrameAmounts(sellType):
 						   YIELD_CHSB: [155.26, 163.82, 167.66, 170.62, 162.54, 712.55, 0.10],
 						   YIELD_CUR_RATE: [0.0] * 7,
 						   REALIZED_CAP_GAIN: [0.0] * 7,
-						   POTENTIAL_CAP_GAIN: [0.0] * 7})
+						   REALIZED_CAP_GAIN_PERCENT: [0.0] * 7,
+						   POTENTIAL_CAP_GAIN: [0.0] * 7,
+						   POTENTIAL_CAP_GAIN_PERCENT: [0.0] * 7})
 	elif sellType == SELL_ALL_DEPOSIT_SELL_ONLY_ONE_YIELD:
 		df = pd.DataFrame({CRYPTO: [10000.0, 10000.0, 1000.0, -500.0, -500, 1000, -21001],
 						   DF_RATE: [5000.0, 10000.0, 2000.0, -500.0, -1000, 800, -25200 - 1.2],
@@ -51,7 +59,9 @@ def computeDataFrameAmounts(sellType):
 						   YIELD_CHSB: [155.26, 163.82, 167.66, 170.62, 162.54, 712.55, 0.10],
 						   YIELD_CUR_RATE: [0.0] * 7,
 						   REALIZED_CAP_GAIN: [0.0] * 7,
-						   POTENTIAL_CAP_GAIN: [0.0] * 7})
+						   REALIZED_CAP_GAIN_PERCENT: [0.0] * 7,
+						   POTENTIAL_CAP_GAIN: [0.0] * 7,
+						   POTENTIAL_CAP_GAIN_PERCENT: [0.0] * 7})
 	elif sellType == SELL_ALL_DEPOSIT_SELL_NO_YIELD:
 		# the last sell is done at CRYPTO/FIAT rate of 1.2 which differs from the current
 		# CRYPTO/FIAT rate of 1.5 !
@@ -62,7 +72,9 @@ def computeDataFrameAmounts(sellType):
 						   YIELD_CHSB: [155.26, 163.82, 167.66, 170.62, 162.54, 712.55, 0.10],
 						   YIELD_CUR_RATE: [0.0] * 7,
 						   REALIZED_CAP_GAIN: [0.0] * 7,
-						   POTENTIAL_CAP_GAIN: [0.0] * 7})
+						   REALIZED_CAP_GAIN_PERCENT: [0.0] * 7,
+						   POTENTIAL_CAP_GAIN: [0.0] * 7,
+						   POTENTIAL_CAP_GAIN_PERCENT: [0.0] * 7})
 
 	print('\n')
 	print(sellType)
@@ -84,6 +96,7 @@ def computeDataFrameAmounts(sellType):
 		yieldCryptoAmount = row[YIELD_CHSB]
 		df.iloc[index][YIELD_CUR_RATE] = yieldCryptoAmount * currentCryptoFiatRate
 		currentTotalYieldCrypto += yieldCryptoAmount
+		currentTotalDepositCryptoAmountAtAverageFiatRate = currentTotalDepositCryptoAmount * currentDepositCryptoAverageFiatRateModifiedOnlyWhenNewPurchase
 
 		if depWithdrCryptoAmount > 0:
 			# we are depositing (purchasing) cryptos ...
@@ -97,10 +110,15 @@ def computeDataFrameAmounts(sellType):
 																				  depWithdrCryptoAmountAtDateFromFiatRate) / \
 																				 currentTotalDepositCryptoAmount
 			potentialCapitalGain = (currentTotalDepositCryptoAmount * currentCryptoFiatRate) + \
-												 (currentTotalYieldCrypto * currentCryptoFiatRate) - \
-												 (currentTotalDepositCryptoAmount * currentDepositCryptoAverageFiatRateModifiedOnlyWhenNewPurchase)
+								   (currentTotalYieldCrypto * currentCryptoFiatRate) - \
+								   currentTotalDepositCryptoAmountAtAverageFiatRate
+			if currentTotalDepositCryptoAmountAtAverageFiatRate != 0:
+				potentialCapitalGainPercent = potentialCapitalGain / \
+											  currentTotalDepositCryptoAmountAtAverageFiatRate * 100
+			else:
+				potentialCapitalGainPercent = 0.0
 		else:
-			# this is a withdrawal ...
+			# this is a withdrawal (or sale)...
 
 			# the realized capital gain is equal to the sold crypro amount converted at the
 			# crypto/fiat rate at the sell date minus the sold crypro amount converted at the
@@ -108,23 +126,40 @@ def computeDataFrameAmounts(sellType):
 			# all the crypto amounts purchased before the sell date.
 			if currentTotalDepositCryptoAmount < 0:
 				withdrawalCryptoAmountWithoutSoldYieldAmount = depWithdrCryptoAmount - currentTotalDepositCryptoAmount
-				df.iloc[index][REALIZED_CAP_GAIN] = -1 * depWithdrCryptoAmountAtDateFromFiatRate - \
-													-1 * withdrawalCryptoAmountWithoutSoldYieldAmount * currentDepositCryptoAverageFiatRateModifiedOnlyWhenNewPurchase
+				realizedCapitalGain = -1 * depWithdrCryptoAmountAtDateFromFiatRate - -1 * withdrawalCryptoAmountWithoutSoldYieldAmount * currentDepositCryptoAverageFiatRateModifiedOnlyWhenNewPurchase
+				df.iloc[index][REALIZED_CAP_GAIN] = realizedCapitalGain
+				realizedCapitalGainPercent = -100 * realizedCapitalGain / \
+											 currentTotalDepositCryptoAmountAtAverageFiatRate
 
 				# the case if not only the current deposit crypto amount has been sold, but also
-				# part or all the accumulated yield crypto amounts
+				# part of or all the accumulated yield crypto amounts
 				potentialCapitalGain = (currentTotalDepositCryptoAmount + currentTotalYieldCrypto) * \
-									    currentCryptoFiatRate
+										currentCryptoFiatRate
+				potentialCapitalGainPercent = -100 * potentialCapitalGain / \
+											  currentTotalDepositCryptoAmountAtAverageFiatRate
 			else:
-				df.iloc[index][REALIZED_CAP_GAIN] = -1 * depWithdrCryptoAmountAtDateFromFiatRate - \
-													-1 * depWithdrCryptoAmount * currentDepositCryptoAverageFiatRateModifiedOnlyWhenNewPurchase
+				realizedCapitalGain = -1 * depWithdrCryptoAmountAtDateFromFiatRate - -1 * depWithdrCryptoAmount * currentDepositCryptoAverageFiatRateModifiedOnlyWhenNewPurchase
+				df.iloc[index][REALIZED_CAP_GAIN] = realizedCapitalGain
 				potentialCapitalGain = (currentTotalDepositCryptoAmount * currentCryptoFiatRate) + \
 									   (currentTotalYieldCrypto * currentCryptoFiatRate) - \
-									   (currentTotalDepositCryptoAmount * currentDepositCryptoAverageFiatRateModifiedOnlyWhenNewPurchase)
+									   currentTotalDepositCryptoAmountAtAverageFiatRate
+				if currentTotalDepositCryptoAmountAtAverageFiatRate != 0:
+					realizedCapitalGainPercent = realizedCapitalGain / \
+												 currentTotalDepositCryptoAmountAtAverageFiatRate * 100
+					potentialCapitalGainPercent = potentialCapitalGain / \
+												  currentTotalDepositCryptoAmountAtAverageFiatRate * 100
+				else:
+					realizedCapitalGainPercent = 0.0
+					potentialCapitalGainPercent = 0.0
 
-		df.iloc[index][POTENTIAL_CAP_GAIN] = round(potentialCapitalGain, 8) # rounding avoid data frame
+			df.iloc[index][REALIZED_CAP_GAIN_PERCENT] = realizedCapitalGainPercent
+
+		df.iloc[index][POTENTIAL_CAP_GAIN] = round(potentialCapitalGain, 8)	# rounding avoid data frame
 																			# potential capital gain values
 																			# exponential formatting
+		df.iloc[index][POTENTIAL_CAP_GAIN_PERCENT] = round(potentialCapitalGainPercent, 8)	# rounding avoid data frame
+																							# potential capital gain values
+																							# exponential formatting
 
 		df.iloc[index][AVERAGE_RATE] = currentDepositCryptoAverageFiatRateModifiedOnlyWhenNewPurchase
 		previousCurrentTotalDepositCryptoAmount = currentTotalDepositCryptoAmount
