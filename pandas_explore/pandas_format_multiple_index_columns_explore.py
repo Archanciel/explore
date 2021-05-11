2d363140555aff4b8a3205dbdf34f1b3376d7440
Archanciel <jp.schnyder@gmail.com>
2021-05-11 07:04:50 +0200

    Update pandas_format_multiple_index_columns_explore.py

--- pandas_explore/pandas_format_multiple_index_columns_explore.py
+++ pandas_explore/pandas_format_multiple_index_columns_explore.py
@@ -15,6 +15,8 @@ print(df.to_string())
 dfp = pd.pivot_table(df, values=["nmem","pct_fem","mean_age","sd_age"], index="recruit_dt", columns="label")
 dfp = dfp.reindex(columns=['nmem', 'pct_fem', 'mean_age', 'sd_age'], level=0)
 
+dfpCopy = dfp.copy()
+
 print(dfp.to_string())
 
 dfp.columns.names = ["metrics","label"]
@@ -23,3 +25,12 @@ dfp.style.format("{:,}", subset=pd.IndexSlice[:,'nmem']) \
 		 .format("{:.2f}", subset=pd.IndexSlice[:,['mean_age','sd_age']])
 
 print(dfp.to_string())
+
+idx = pd.IndexSlice
+formatter_dict = {i:"{:,}" for i in dfpCopy.loc[:, idx['nmem', :]].columns}
+formatter_dict2 = {i:"{:.2%}" for i in dfpCopy.loc[:, idx['pct_fem', :]].columns}
+formatter_dict3 = {i:"{:.2f}" for i in dfpCopy.loc[:, idx[['mean_age', 'sd_age'], :]].columns}
+formatter_dict.update(formatter_dict2)
+formatter_dict.update(formatter_dict3)
+dfpCopy.style.format(formatter_dict)
+print(dfpCopy.to_string())
