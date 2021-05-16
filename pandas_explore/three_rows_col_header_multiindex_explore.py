b6826b0c4386b7bb714d978560341b2d1a2e17aa
Archanciel <jp.schnyder@gmail.com>
2021-05-16 16:44:57 +0200

    added getting multi index row values

--- pandas_explore/three_rows_col_header_multiindex_explore.py
+++ pandas_explore/three_rows_col_header_multiindex_explore.py
@@ -3,7 +3,7 @@ import numpy as np
 
 OWNER = 'OWNER'
 DEPWITHDR = 'DEP/WITHDR'
-CAPITAL = ''
+CAPITAL = '  '
 CHSB_DP_I = 'CHSB'
 CHF_DP_I = 'CHF'
 CHF_DP_C = ' CHF'
@@ -18,11 +18,11 @@ CURR = 'CURR RATE'
 
 df = pd.DataFrame({
 	OWNER: 4*['JPS']+2*['Papa'],
-	CHSB_DP_I: [4422, 511, 2047, 300, 15941, 8973],
-	CHF_DP_I: [2287, 366, 2867, 420, 15941, 8973],
+	CHSB_DP_I: [4422, 511, 2047, 300, 15641, 8973],
+	CHF_DP_I: [2287, 366, 2867, 420, 15241, 8973],
 	CHF_DP_C: [4152, 479, 1930, 284, 15941, 8973],
-	CHSB_C_C: [4422, 4948, 7009, 7312, 15941, 8973],
-	CHF_C_C: [4152, 4704, 6688, 6978, 15941, 8973],
+	CHSB_C_C: [4422, 4948, 7009, 7312, 15741, 8973],
+	CHF_C_C: [4152, 4704, 6688, 6978, 15041, 8973],
 	CHSB_Y: [14, 12, 2, 27, 92, 106],
 	CHF_Y: [13, 11, 1.9, 25, 92, 106]
 	})
@@ -40,3 +40,13 @@ tuples = list(zip(*arrays))
 index = pd.MultiIndex.from_tuples(tuples)
 df.columns = index
 print(df)
+print('iloc[4][DEPWITHDR]: ', df.iloc[4][DEPWITHDR])
+print('iloc[3]: ', df.iloc[3])
+print('iloc[0][4]: ', df.iloc[0][4])
+print('iloc[0][CAPITAL]: ', df.iloc[0][CAPITAL])
+print()
+print('INTERESTING !!!')
+print("REFERRING LEVEL 0 ONLY: iloc[1][' ']:\n", df.iloc[1][' '])
+print("REFERRING LEVEL 0 AND 1 ONLY: iloc[1][' '][INIT]:\n", df.iloc[1][' '][INIT])
+print("REFERRING LEVEL 0, 1 AND 3: iloc[1][' ']['CURR'][CHSB_C_C]\n", df.iloc[1][' ']['CURR'][CHSB_C_C])
+print("REFERRING LEVEL 0, 1 AND 3: iloc[1][' ']['CURR'][CHSB_Y]\n", df.iloc[1][' ']['CURR'][CHSB_Y])
