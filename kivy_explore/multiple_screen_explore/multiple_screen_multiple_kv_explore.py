1b8afc9054794edcee2f8498d3a5e33f00122e30
Archanciel <jp.schnyder@gmail.com>
2021-07-03 12:50:49 +0200

    tested AudioDownload structure

--- kivy_explore/multiple_screen_explore/multiple_screen_multiple_kv_explore.py
+++ kivy_explore/multiple_screen_explore/multiple_screen_multiple_kv_explore.py
@@ -2,11 +2,14 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 
+from kivy.core.clipboard import Clipboard
+
 # tutorial video: https://youtu.be/xaYn4XdieCs
 
 class MainWindow(Screen):
-	pass
-
+	def printClipboard(self, clipboardContent):
+		print('Clipboard content\n')
+		print(clipboardContent)
 
 class SecondWindow(Screen):
 	pass
@@ -22,8 +25,16 @@ class MyMainApp(App):
 		Builder.load_file("secondwindow.kv")
 		kv = Builder.load_file("windowmanager.kv")
 		
+		self.mainWindow = MainWindow()
+		
 		return kv
 
+	def on_start(self):
+		print("Executing MyMainApp.on_start()")
+		
+		clipboardContent = Clipboard.paste()
+
+		self.mainWindow.printClipboard(clipboardContent)
 
 if __name__ == "__main__":
 	MyMainApp().run()
