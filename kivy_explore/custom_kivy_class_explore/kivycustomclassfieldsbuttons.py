68d79f5641796c33db159c472d62992e4c222d7d
Archanciel <jp.schnyder@gmail.com>
2021-08-01 20:31:47 +0200

    Update kivycustomclassfieldsbuttons.py

--- kivy_explore/custom_kivy_class_explore/kivycustomclassfieldsbuttons.py
+++ kivy_explore/custom_kivy_class_explore/kivycustomclassfieldsbuttons.py
@@ -2,6 +2,9 @@ from kivy.uix.boxlayout import BoxLayout
 
 from kivy_explore.custom_kivy_class_explore.kivycustomclassbuttons import KivyCustomClassButtons
 
+# class KivyCustomClassFieldsButtons(KivyCustomClassButtons):   # inheriting from KivyCustomClassButtons
+																# does cause double display of tet fields
+																# and buttons !!!
 
 class KivyCustomClassFieldsButtons(BoxLayout):
 	def __init__(self, **kwargs):
