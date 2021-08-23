9073a5265df68e67e4a0e4a29e5709d642a1063d
Archanciel <jp.schnyder@gmail.com>
2021-08-23 16:03:04 +0200

    dynamically size TextInput

--- kivy_explore/textinputtextsize.py
+++ kivy_explore/textinputtextsize.py
@@ -5,28 +5,12 @@ from kivy.uix.boxlayout import BoxLayout
 
 class TextInputTextSize(BoxLayout):
     textInput = ObjectProperty()
-    textOutput = ObjectProperty()
+    textInputMultiline = ObjectProperty()
 
     def submitRequest(self):
         # Get the request from the TextInput
-        textInputTxt = self.textInput.text
-        self.textOutput.text = textInputTxt
-        textSize = self.textOutput._lines_labels[0].width
-        print(textSize)
-
-        if textSize > 392:
-            self.textOutput.font_size -= 3
-            self.textOutput.text = ''
-            self.textOutput.text = textInputTxt
-            textSize = self.textOutput._lines_labels[0].width
-            print(textSize)
-
-        self.textOutput.text = textInputTxt
-
-    def outputTextChanged(self, text_input, text):
-        if len(text) > 0:
-            text_size = text_input._lines_labels[0].width
-            print('font size: ', text_input.font_size, ' ', text, ': ', text_size)
+        textInputTxt = self.textInputSingleLine.text
+        self.textInputMultiline.text += '\n' + textInputTxt
 
 class TextInputTextSizeApp(App):
     def build(self):
