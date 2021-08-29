a4dbc50911ca4840326747421d92533282ff9d55
Archanciel <jp.schnyder@gmail.com>
2021-08-29 20:53:52 +0200

    Create moviepy_extract_audip_from_mp4.py

---
+++ youtube_audio_explore/moviepy_extract_audip_from_mp4.py
@@ -0,0 +1,88 @@
+import time, glob, os
+import moviepy.editor
+from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_audio
+from playsound import playsound
+
+PLAY = False
+AUDIO_DIR = 'D:\\Users\\Jean-Pierre\\Downloads\\Audiobooks\\test'
+DIR_SEP = '\\'
+
+testDirName = 'audible_mobizen'
+targetAudioDir = AUDIO_DIR + DIR_SEP + testDirName
+sourceName = "Audible Et l'Uunivers disparaîtra"
+sourceName = "Short low video quality"
+sourceVideoFileName = sourceName + ".mp4"
+sourceVideoFilePath = targetAudioDir + DIR_SEP + sourceVideoFileName
+
+# deleting audio files in audio dir
+files = glob.glob(targetAudioDir + DIR_SEP + '*.mp3')
+
+for f in files:
+	os.remove(f)
+
+ts = time.time()
+audio = moviepy.editor.AudioFileClip(sourceVideoFilePath, fps=22050)
+targetAudioFileName = sourceName + "_moviepy_22050.mp3"
+targetAudioFilePath = targetAudioDir + DIR_SEP + targetAudioFileName
+audio.write_audiofile(targetAudioFilePath)
+audio.close()
+
+print('\nmoviepy.editor.AudioFileClip({}, fps=22050)\n'.format(sourceVideoFilePath))
+audioExtractionTime = str(time.time() - ts)
+print(audioExtractionTime) # 37.5 sec
+newAudioFilePath = targetAudioFilePath[:-4] + '_' + audioExtractionTime + '.mp3'
+os.rename(targetAudioFilePath, newAudioFilePath)
+
+if PLAY:
+	playsound(newAudioFilePath)
+	input("Type any key to continue ...")
+
+
+targetAudioFileName = sourceName + "_ffmpeg_64_44100.mp3"
+targetAudioFilePath = targetAudioDir + DIR_SEP + targetAudioFileName
+ts = time.time()
+
+# fps=44100 ---> 34.5 sec
+ffmpeg_extract_audio(sourceVideoFilePath, targetAudioFilePath, bitrate=64, fps=44100)
+print('\nffmpeg_extract_audio({}, {}, bitrate=64, fps=44100)\n'.format(sourceVideoFilePath, targetAudioFilePath))
+audioExtractionTime = str(time.time() - ts)
+print(audioExtractionTime) # 37.5 sec
+newAudioFilePath = targetAudioFilePath[:-4] + '_' + audioExtractionTime + '.mp3'
+os.rename(targetAudioFilePath, newAudioFilePath)
+
+if PLAY:
+	playsound(newAudioFilePath)
+	input("Type any key to continue ...")
+
+targetAudioFileName = sourceName + "_ffmpeg_64_22050.mp3"
+targetAudioFilePath = targetAudioDir + DIR_SEP + targetAudioFileName
+ts = time.time()
+
+# fps=22050 ---> 19.7 sec quality ok !
+# THIS THE OPTIMALEST AUDIO  EXTRACTION
+ffmpeg_extract_audio(sourceVideoFilePath, targetAudioFilePath, bitrate=64, fps=22050)
+print('\nffmpeg_extract_audio({}, {}, bitrate=64, fps=22050)\n'.format(sourceVideoFilePath, targetAudioFilePath))
+audioExtractionTime = str(time.time() - ts)
+print(audioExtractionTime) # 37.5 sec
+newAudioFilePath = targetAudioFilePath[:-4] + '_' + audioExtractionTime + '.mp3'
+os.rename(targetAudioFilePath, newAudioFilePath)
+
+if PLAY:
+	playsound(newAudioFilePath)
+	input("Type any key to continue ...")
+
+targetAudioFileName = sourceName + "_ffmpeg_128_22050.mp3"
+targetAudioFilePath = targetAudioDir + DIR_SEP + targetAudioFileName
+ts = time.time()
+
+# fps=22050 ---> 19.7 sec quality ok !
+ffmpeg_extract_audio(sourceVideoFilePath, targetAudioFilePath, bitrate=128, fps=22050)
+print('\nffmpeg_extract_audio({}, {}, bitrate=128, fps=22050)\n'.format(sourceVideoFilePath, targetAudioFilePath))
+audioExtractionTime = str(time.time() - ts)
+print(audioExtractionTime) # 37.5 sec
+newAudioFilePath = targetAudioFilePath[:-4] + '_' + audioExtractionTime + '.mp3'
+os.rename(targetAudioFilePath, newAudioFilePath)
+
+if PLAY:
+	playsound(newAudioFilePath)
+	input("Type any key to continue ...")
