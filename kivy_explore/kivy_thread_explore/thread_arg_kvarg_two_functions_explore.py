7672fafef5b7ffad300225b7e9735f966449612e
Archanciel <jp.schnyder@gmail.com>
2021-10-08 18:28:12 +0200

    Update thread_arg_kvarg_two_functions_explore.py

--- kivy_explore/kivy_thread_explore/thread_arg_kvarg_two_functions_explore.py
+++ kivy_explore/kivy_thread_explore/thread_arg_kvarg_two_functions_explore.py
@@ -1,37 +1,37 @@
 import threading, time
 
 def myFunc(name='', age=0):
-	print('My name is {}. I am {} years old'.format(name, age))
+	for i in range(5):
+		print('My name is {}. I am {} years old'.format(name, age))
+		time.sleep(1)
 
 
 def myEndFunc(name='', age=0):
 	print('MY SURNAME WAS {}. I was {} years old'.format(name.upper(), age))
 
 
-def startWithCallback(func, endFunc, endFuncArgs=None, kwargs=None):
+def startWithCallback(func, endFunc, funcArgs=None, endFuncArgs=None):
 	"""Start 'func' in a new thread T, then start self (and return T)."""
 	if endFuncArgs is None:
 		endFuncArgs = ()
-	if kwargs is None:
-		kwargs = {}
+	if funcArgs is None:
+		funcArgs = {}
 		
 	args = (func, endFunc) + endFuncArgs
 	
 	def _callback(func, endFunc, *a, **kw):
-		for i in range(5):
-			time.sleep(1)
-			func(**kw)
-			
+		func(**kw)
 		endFunc(*a)
 	
-	t = threading.Thread(target=_callback, args=args, kwargs=kwargs)
+	t = threading.Thread(target=_callback, args=args, kwargs=funcArgs)
 	t.setName('Exec thread ' + t.getName())
 	t.daemon = True
 	t.start()
 	
 	return t
 
-thread = startWithCallback(func=myFunc, endFunc=myEndFunc, endFuncArgs=('paulo le scientifique', 14), kwargs={'name': 'Jean-Pierre', 'age': 60})
+thread = startWithCallback(func=myFunc, endFunc=myEndFunc, funcArgs={'name': 'Jean-Pierre', 'age': 60},
+                           endFuncArgs=('paulo le scientifique', 14))
 print(thread.getName(), 'started ...')
 time.sleep(6)   # necessary, otherwise, since the daemon thread property is
 				# set to True, the program will end and suppress the thread
