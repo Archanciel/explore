60d844f28472ae8bc77c5abbfad1df05a806da3f
Archanciel <jp.schnyder@gmail.com>
2021-10-08 18:44:44 +0200

    working

--- kivy_explore/kivy_thread_explore/septhreadexec.py
+++ kivy_explore/kivy_thread_explore/septhreadexec.py
@@ -1,5 +1,39 @@
-import threading
+import threading, time
 
 class SepThreadExec:
-	def __init__(self, callerGUI, execFunction, endFunction):
-		pass
+	def __init__(self, callerGUI, func, endFunc, funcArgs=None, endFuncArgs=None):
+		if endFuncArgs is None:
+			endFuncArgs = ()
+		if funcArgs is None:
+			funcArgs = {}
+		
+		args = (func, endFunc) + endFuncArgs
+		
+		def _callback(func, endFunc, *a, **kw):
+			func(**kw)
+			endFunc(*a)
+		
+		self.t = threading.Thread(target=_callback, args=args, kwargs=funcArgs)
+		self.t.setName('Exec thread ' + self.t.getName())
+		self.t.daemon = True
+		
+	def start(self):
+		self.t.start()
+
+if __name__ == "__main__":
+	def myFunc(name='', age=0):
+		for i in range(5):
+			time.sleep(1)
+			print('My name is {}. I am {} years old'.format(name, age))
+	
+	def myEndFunc(name='', age=0):
+		print('MY SURNAME WAS {}. I was {} years old'.format(name.upper(), age))
+		
+	ste = SepThreadExec(callerGUI=None,
+	              func=myFunc,
+	              endFunc=myEndFunc,
+	              funcArgs={'name': 'Jean-Pierre', 'age': 60},
+	              endFuncArgs=('paulo le scientifique', 14))
+	
+	ste.start()
+	time.sleep(6)
