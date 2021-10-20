6e184a566650adc88a49b0f831ba7902d06e0609
Archanciel <jp.schnyder@gmail.com>
2021-10-20 15:21:29 +0200

    Update youtube-dl_explore.py

--- youtube_audio_explore/youtube-dl_explore.py
+++ youtube_audio_explore/youtube-dl_explore.py
@@ -4,16 +4,16 @@ from pytube import Playlist
 
 PRINT_SECONDS = 1
 
-class DownloadInfoPrinter:
+class YdlDownloadInfoExtractor:
 	def __init__(self):
-		self.lstPrintTime = time.time()
+		self.lstExtractTime = time.time()
 	
-	def callable_hook(self, response):
+	def ydlCallableHook(self, response):
 		if response['status'] == 'downloading':
 			now = time.time()
-			if now - self.lstPrintTime >= PRINT_SECONDS:
+			if now - self.lstExtractTime >= PRINT_SECONDS:
 				print('download bytes {}: {} %. Speed: {}'.format(response["downloaded_bytes"], response["_percent_str"], response["_speed_str"]))
-				self.lstPrintTime = now
+				self.lstExtractTime = now
 		elif response['status'] == 'finished':
 			print('total download bytes {}. Now, converting to mp3 ...'.format(response["total_bytes"]))
 	
@@ -36,7 +36,7 @@ else:
 							'preferredcodec': 'mp3',
 							'preferredquality': '96',
 						}],
-		"progress_hooks": [DownloadInfoPrinter().callable_hook],
+		"progress_hooks": [YdlDownloadInfoExtractor().ydlCallableHook],
 		'quiet': True
 	}
 
