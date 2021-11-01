344647a23316d926e55f6df61c2d21da40b91d15
Archanciel <jp.schnyder@gmail.com>
2021-11-01 20:37:35 +0100

    Create composite_interest_explore.py

---
+++ numpy_explore/composite_interest_explore.py
@@ -0,0 +1,13 @@
+import math
+
+class CompositeInterest:
+	@staticmethod
+	def computeDayNumber(capital, yearlyRatePercent, withdrawCostPercent, withdrawCostMinimum, withdrawCostMaximum):
+		daylyRate = pow(1 + yearlyRatePercent / 100, 1/365)
+		withdrawCost = min(max(capital * withdrawCostPercent / 100,withdrawCostMinimum), withdrawCostMaximum)
+		dayNumber = math.log10((capital + withdrawCost) / capital) / math.log10(daylyRate)
+		
+		return math.ceil(dayNumber)
+		
+if __name__ == '__main__':
+    print(CompositeInterest.computeDayNumber(800, 10.5, 0.1, 4.5, 110))
