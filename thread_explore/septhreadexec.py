f0003af4de509fc748d6025b295642043f36a071
Archanciel <jp.schnyder@gmail.com>
2021-11-08 17:23:45 +0100

    Replaced Thread by Process

--- thread_explore/septhreadexec.py
+++ thread_explore/septhreadexec.py
@@ -1,4 +1,5 @@
 import threading, time
+from multiprocessing import Process
 
 class SepThreadExec:
 	def __init__(self, callerGUI, func, endFunc, funcArgs=None, endFuncArgs=None):
@@ -11,47 +12,43 @@ class SepThreadExec:
 		
 		args = (func, endFunc) + endFuncArgs
 		
-		def _callback(func, endFunc, *a, **kw):
-			func(**kw)
-			endFunc(*a)
-		
-		self.t = threading.Thread(target=_callback, args=args, kwargs=funcArgs)
-		self.t.setName('Exec thread ' + self.t.getName())
-		self.t.daemon = True
-		
+		self.p = Process(target=self._callback, args=args, kwargs=funcArgs)
+		self.p.daemon = True
+	
+	def _callback(self, func, endFunc, *a, **kw):
+		func(**kw)
+		endFunc(*a)
+	
 	def start(self):
-		self.callerGUI.displayMsg(self.t.getName() + ' started ...')
-		self.t.start()
+		self.callerGUI.displayMsg('Process started ...')
+		self.p.start()
+		
+	def stop(self):
+		self.p.terminate()
 
 class GUIStub:
-	def __init__(self):
-		self.stop = False
-		
 	def displayMsg(self, msg):
 		print('GUIStub' + ' ' + msg)
 		
 	def myFunc(self, name='', age=0):
 		for i in range(5):
-			if not self.stop:
-				time.sleep(1)
-				print('My name is {}. I am {} years old'.format(name, age))
-			else:
-				print('stopping ...')
-				break
-			
+			time.sleep(1)
+			print('{}: My name is {}. I am {} years old'.format(i + 1, name, age))
+
+def myEndFunc(name='', age=0):
+	print('MY SURNAME WAS {}. I was {} years old'.format(name.upper(), age))
+
 if __name__ == "__main__":
-	def myEndFunc(name='', age=0):
-		print('MY SURNAME WAS {}. I was {} years old'.format(name.upper(), age))
 	
 	gui = GUIStub()
-		
+	
 	ste = SepThreadExec(callerGUI=gui,
-	              func=gui.myFunc,
-	              endFunc=myEndFunc,
-	              funcArgs={'name': 'Jean-Pierre', 'age': 60},
-	              endFuncArgs=('paulo le scientifique', 14))
+	                    func=gui.myFunc,
+	                    endFunc=myEndFunc,
+	                    funcArgs={'name': 'Jean-Pierre', 'age': 60},
+	                    endFuncArgs=('paulo le scientifique', 14))
 	
 	ste.start()
 	time.sleep(3)
-	gui.stop = True
+	#ste.stop()
 	time.sleep(3)
